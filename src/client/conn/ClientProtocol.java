c65d3ff014403a5ad2f3d10d6a8dbdc834f05875
Flor Kaucic <florkaucic@gmail.com>
2015-11-06 21:07:46 -0300

    Protocol

--- src/client/conn/ClientProtocol.java
+++ src/client/conn/ClientProtocol.java
@@ -1,5 +1,25 @@
 package client.conn;
 
 public class ClientProtocol {
-
+	// Registrar usuario:
+		// envia
+		// LOGUP username password
+		// recibe
+		// LOGUPOK PACMAN/GHOST id nickname
+		// LOGUPFAILED [...]
+		
+		// Ingresar usuario:
+		// envia
+		// LOGIN username password
+		// recibe
+		// LOGINOK PACMAN/GHOST id nickname
+		// LOGINFAILED [...]
+			
+		
+		public static void processInput(String input){
+			if(input.startsWith("LOGUPOK") || input.startsWith("LOGINOK")){
+				Connection.getInstance().setStatus("LOGGED");
+			}
+			Connection.getInstance().setStatus("FAILED");
+		}	
 }
